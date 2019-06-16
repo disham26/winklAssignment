9f1a4c89c3b3b5a1af80b48dfcbd6edd89291506
prasangmisra01 <prasang@ruvi.in>
2019-06-16 08:37:16 +0530

    styling done, search working on tap

--- winkl_assignment/lib/main.dart
+++ winkl_assignment/lib/main.dart
@@ -4,13 +4,17 @@ import 'package:flutter/semantics.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'models/models.dart' as models;
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        ExtractArgumentsScreen.routeName: (context) => ExtractArgumentsScreen(),
+      },
       title: "WinklDemo",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -21,203 +25,331 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class FirstPage extends StatefulWidget{  
-    _FirstPage createState() => _FirstPage();
-
+class FirstPage extends StatefulWidget {
+  _FirstPage createState() => _FirstPage();
 }
 
-
-class _FirstPage extends State<FirstPage>{
+class _FirstPage extends State<FirstPage> {
   //var _categoryNameController = new TextEditingController();
-  bool search=false;
-  String queryTerm="";
-  String hintForTextSearch="Search";
+  bool search = false;
+  String queryTerm = "";
+  String hintForTextSearch = "Search";
   FocusNode _searchTextFocus = new FocusNode();
   List<models.Pictures> images = new List();
   ScrollController _scrollController = new ScrollController();
   TextEditingController _searchController = TextEditingController();
-  bool showLoading=false;
-  var page=1;
+  bool showLoading = false;
+  bool showCancel = false;
+  bool hasContent = false;
+  var page = 1;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     fetch();
-    _searchController.addListener(onChange); 
+    _searchController.addListener(onChange);
     _searchTextFocus.addListener(onChange);
-    _scrollController.addListener((){
-      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
-        if(!search){
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        if (!search) {
           fetch();
-          }else{
-            fetchSearch(queryTerm);
-          }
+        } else {
+          fetchSearch(queryTerm);
+        }
       }
     });
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _scrollController.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context){
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(statusBarColor:Colors.black));
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.black));
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: Container(
-          margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 8.0),
-          decoration: BoxDecoration(
-            color: Color.fromARGB(50, 255, 255, 255),
-            borderRadius: BorderRadius.all(Radius.circular(12.0)),
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Container(
+            margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(50, 255, 255, 255),
+              borderRadius: BorderRadius.all(Radius.circular(12.0)),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
                 showLoading
-                ?
-                IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: (){
-                  setState(() {
-                    _searchController.clear();
-                    images.clear();
-                    page=1;
-                    search=false;
-                    fetch();
-                    showLoading=false;
-                  });
-                },
-                ): 
-                Text('' ),
-              
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding:EdgeInsets.symmetric(horizontal: 5.0),
-                  child:TextFormField(
-                    
-                    controller: _searchController,
-                    focusNode: _searchTextFocus,
-                    onFieldSubmitted: (text){
-                      setState(() {
-                        images.clear();
-                        page=1;
-                        fetchSearch(text);
-                      });
-                      
-                    },
-                    decoration: InputDecoration(
-                    border: InputBorder.none,       
-                    hintText: hintForTextSearch,
-                    hintStyle: TextStyle(color: Colors.black),
-                    icon: Icon(Icons.search,color: Colors.black,),
+                    ? IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () {
+                          setState(() {
+                            _searchController.clear();
+                            images.clear();
+                            page = 1;
+                            search = false;
+                            fetch();
+                            showLoading = false;
+                          });
+                        },
+                      )
+                    : SizedBox(),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: TextFormField(
+                      controller: _searchController,
+                      focusNode: _searchTextFocus,
+                      onFieldSubmitted: (text) {
+                        setState(() {
+                          images.clear();
+                          page = 1;
+                          fetchSearch(text);
+                        });
+                      },
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: hintForTextSearch,
+                        hintStyle: TextStyle(color: Colors.black),
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                ),
-              ),
-              IconButton(
-                icon: Icon(Icons.cancel),
-                onPressed: (){
-                  setState(() {
-                    _searchController.clear();
-                  });
-                },
-                ),
-              
-              // SizedBox(height: 32),
-              // Divider(),    
-              // SizedBox(height: 32),
-            ],
+                showCancel
+                    ? IconButton(
+                        icon: Icon(Icons.cancel),
+                        onPressed: () {
+                          setState(() {
+                            _searchController.clear();
+                          });
+                        },
+                      )
+                    : SizedBox(),
+              ],
+            ),
           ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-    ),body: 
-            ListView.builder(
-              
-              controller: _scrollController,
-              itemCount: images.length,
-              itemBuilder: (BuildContext context, int index){
-                return Card(
-                  elevation: 2,
-                  child: Container(
-                    child: Row(
-                      children: <Widget>[
-                        Image.network(images[index].link,fit:BoxFit.scaleDown),
-                        Spacer(flex: 2,),
-                        Image.network(images[index].link,fit:BoxFit.fitHeight),
-                       
-                        ],
-                    ),
-                  )
-                  
-                );
-              },
-            )
-        
-        );
-  }
-void onChange(){
-  print("Onchange called-------------");
-  String text = _searchController.text;
-  bool hasFocus = _searchTextFocus.hasFocus;
-  //do your text transforming
-  String newText = "Search Anything";
-  _searchController.text = newText;
-  _searchController.selection = new TextSelection(
-                                baseOffset: newText.length, 
-                                extentOffset: newText.length
+        body: new Column(
+          children: <Widget>[
+            new Expanded(
+                child: new GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 3.0,
+                        mainAxisSpacing: 3.0),
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (BuildContext context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            ExtractArgumentsScreen.routeName,
+                            arguments: ScreenArguments(images[index]),
                           );
-}
-  fetch() async{
-    final response = await http.get('https://api.unsplash.com/photos?client_id=641f7742e10311edcb08d2df0ef6cc2600b2868c0d0872bc7e1df277cba259bd&per_page=30&page='+page.toString());
-    print("Status response:"+response.statusCode.toString());
-    if(response.statusCode == 200){
-      setState((){
+                        },
+                        child: Card(
+                          child: Image.network(
+                            images[index].link,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                    controller: _scrollController,
+                    itemCount: images.length)),
+          ],
+        ));
+  }
+
+  void onChange() {
+    String text = _searchController.text;
+    if (_searchController.text.length > 0) {
+      setState(() {
+        showCancel = true;
+      });
+    } else {
+      setState(() {
+        showCancel = false;
+      });
+    }
+    bool hasFocus = _searchTextFocus.hasFocus;
+    //do your text transforming
+
+    // _searchController.text = newText;
+    // _searchController.selection = new TextSelection(
+    //                               baseOffset: newText.length,
+    //                               extentOffset: newText.length
+    //                         );
+  }
+
+  fetch() async {
+    final response = await http.get(
+        'https://api.unsplash.com/photos?client_id=641f7742e10311edcb08d2df0ef6cc2600b2868c0d0872bc7e1df277cba259bd&per_page=30&page=' +
+            page.toString());
+    if (response.statusCode == 200) {
+      hasContent = false;
+      setState(() {
         page++;
         List jsonData = json.decode(response.body);
-        jsonData.forEach((i){
-            models.Pictures pictures = models.Pictures(
+        jsonData.forEach((i) {
+          hasContent = true;
+          models.Pictures pictures = models.Pictures(
               alt_description: i['alt_description'],
               id: i['id'],
-              link: i['urls']['thumb']
-            );
-            images.add(pictures);
-          });
+              link: i['urls']['thumb'],
+              author_bio: i['user']['bio'],
+              author_name: i['user']['name'],
+              linkFull: i['urls']['raw'],
+              height: i['height'],
+              width: i['width']);
+          images.add(pictures);
         });
-    }
-    else{
+      });
+    } else {
       throw Exception('Failed to load image');
     }
   }
 
-
-  fetchSearch(String text) async{
-    queryTerm=text;
-    String query = 'https://api.unsplash.com/search/photos?client_id=641f7742e10311edcb08d2df0ef6cc2600b2868c0d0872bc7e1df277cba259bd&per_page=30&page='+page.toString()+'&query='+text;
-    print(query);
+  fetchSearch(String text) async {
+    queryTerm = text;
+    String query =
+        'https://api.unsplash.com/search/photos?client_id=641f7742e10311edcb08d2df0ef6cc2600b2868c0d0872bc7e1df277cba259bd&per_page=30&page=' +
+            page.toString() +
+            '&query=' +
+            text;
     final response = await http.get(query);
-    print("Status response:"+response.statusCode.toString());
-    if(response.statusCode == 200){
-      setState((){
-        showLoading=true;
+    if (response.statusCode == 200) {
+      setState(() {
+        showLoading = true;
         page++;
         List jsonData = json.decode(response.body)['results'];
-        jsonData.forEach((i){
-            models.Pictures pictures = models.Pictures(
+        jsonData.forEach((i) {
+          models.Pictures pictures = models.Pictures(
               alt_description: i['alt_description'],
               id: i['id'],
-              link: i['urls']['thumb']
-            );
-            images.add(pictures);
-          });
+              link: i['urls']['thumb']);
+          images.add(pictures);
         });
-    }
-    else{
+      });
+    } else {
       throw Exception('Failed to load image');
     }
   }
+}
+
+class ExtractArgumentsScreen extends StatelessWidget {
+  static const routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute settings and cast
+    // them as ScreenArguments.
+    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Picture by ' +
+              (args.picture.author_name == null
+                  ? "someone"
+                  : args.picture.author_name)),
+        ),
+        body: 
+
+        SingleChildScrollView(
+          padding: const EdgeInsets.only(left: 18.0, right: 18.0,top:20.0,bottom:20.0),
+                scrollDirection: Axis.vertical,
+              child: Flex(direction: Axis.horizontal, children: <Widget>[
+                Expanded(
+                    child: Column(children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: args.picture.linkFull == null
+                        ? CachedNetworkImage(
+                            imageUrl: args.picture.link,
+                            placeholder: (context, url) =>
+                                new CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                new Icon(Icons.error),
+                          )
+                        : CachedNetworkImage(
+                            imageUrl: args.picture.linkFull,
+                            placeholder: (context, url) =>
+                                new CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                new Icon(Icons.error),
+                          ),
+                  ),
+                  args.picture.alt_description == null
+                      ? SizedBox()
+                      : Align(
+                        alignment: Alignment.topLeft,
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: RichText(
+                              text: new TextSpan(
+                            children: <TextSpan>[
+                              new TextSpan(
+                                  text: "Description:",
+                                  style: new TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black)),
+                              new TextSpan(
+                                  text: args.picture.alt_description,
+                                  style: new TextStyle(color: Colors.black))
+                            ],
+                          )),
+                        ),
+                      ),
+                  args.picture.author_bio != null
+                      ? Align(
+                        
+                          alignment: Alignment.topLeft,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 20),
+                            child: new RichText(
+                              text: new TextSpan(children: <TextSpan>[
+                            new TextSpan(
+                                text: "Something about the author: ",
+                                style: new TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black)),
+                            new TextSpan(
+                                text: args.picture.author_bio == null
+                                    ? "The author has not updated the bio yet"
+                                    : args.picture.author_bio,
+                                style: new TextStyle(color: Colors.black))
+                          ]))))
+                      : SizedBox()
+                    ]
+                      )
+                )
+                
+              ]
+              ),
+)
+            );
+  }
+}
+
+class ScreenArguments {
+  final models.Pictures picture;
+
+  ScreenArguments(this.picture);
+}
+
+isNull(String input) {
+  if (input.length == 0) {
+    return true;
+  }
+  return false;
 }
--- winkl_assignment/lib/models/models.dart
+++ winkl_assignment/lib/models/models.dart
@@ -4,5 +4,10 @@ class Pictures {
   final String id;
   final String alt_description;
   final String link;
-  Pictures({this.alt_description,this.id,this.link});
+  final String linkFull;
+  final String author_name;
+  final int height;
+  final int width;
+  final String author_bio;
+  Pictures({this.alt_description,this.id,this.link,this.author_bio,this.author_name,this.linkFull,this.height,this.width});
 }
